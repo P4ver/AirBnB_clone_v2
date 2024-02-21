5c42b7d3ddc044c9730071ae25a57cbc6c244790
P4ver <s.pales20@gmail.com>
2024-02-21 13:52:00 -0800

    tsk

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This is the file storage module for the AirBnB project."""
+"""This is the file storage class for the AirBnB project."""
 import json
+import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,12 +9,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-import shlex
 
 
 class FileStorage:
     """This class manages serialization of instances to a JSON file and
-    deserialization of JSON file to instances.
+    deserialization of JSON file to instances
     Attributes:
         __file_path: A string representing the path to the JSON file.
         __objects: A dictionary containing objects stored by their unique IDs.
@@ -28,16 +28,10 @@ class FileStorage:
         Returns:
             dict: A dictionary of objects or objects filtered by class.
         """
-        obj_dict = {}
         if cls:
-            obj_dict = {
-                key: obj
-                for key, obj in self.__objects.items()
-                if isinstance(obj, cls)
-            }
-        else:
-            obj_dict = self.__objects
-        return obj_dict
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, type(cls))}
+        return self.__objects
 
     def new(self, obj):
         """Adds a new object to __objects.
@@ -50,25 +44,90 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects and saves to the JSON file."""
-        serializable_objs = {
-            key: obj.to_dict() for key, obj in self.__objects.items()
-        }
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
         with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(serializable_objs, f)
+            json.dump(my_dict, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                deserialized_objs = json.load(f)
-                for key, value in deserialized_objs.items():
+                for key, value in (json.load(f)).items():
                     value = eval(value["__class__"])(**value)
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes an object from __objects if it exists.
+        """Deletes obj if it's inside the attribute __objects.
         Args:
             obj: The object to be deleted.
         """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if (key, obj) in self.__objects.items():
+                self.__objects.pop(key, None)
+        self.save()
+
+    def close(self):
+        """Deserializes the JSON file to objects."""
+        self.reload()
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
